d345924c32ab69f544580de7874c19ab494099d4
German Boldyrev <boldyrev_german@mail.ru>
2025-10-25 02:56:18 +0300

    feat: фильтра

--- app/JsonApi/V1/Categories/CategorySchema.php
+++ app/JsonApi/V1/Categories/CategorySchema.php
@@ -2,12 +2,14 @@
 
 namespace App\JsonApi\V1\Categories;
 
+use App\JsonApi\V1\Filters\WhereLike;
 use App\Models\Category;
 use LaravelJsonApi\Eloquent\Contracts\Paginator;
 use LaravelJsonApi\Eloquent\Fields\DateTime;
 use LaravelJsonApi\Eloquent\Fields\ID;
 use LaravelJsonApi\Eloquent\Fields\Relations\HasMany;
 use LaravelJsonApi\Eloquent\Fields\Str;
+use LaravelJsonApi\Eloquent\Filters\Where;
 use LaravelJsonApi\Eloquent\Filters\WhereIdIn;
 use LaravelJsonApi\Eloquent\Pagination\PagePagination;
 use LaravelJsonApi\Eloquent\Schema;
@@ -50,6 +52,10 @@ class CategorySchema extends Schema
     {
         return [
             WhereIdIn::make($this),
+            Where::make('name'),
+
+            // Кастомный поиск
+            WhereLike::make('search', 'name'),
         ];
     }
 
