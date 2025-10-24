3d984834740a9d50c478a458bf38a5e5eb2a7e1f
German Boldyrev <boldyrev_german@mail.ru>
2025-10-24 21:47:57 +0300

    feat: подготовка под новые поля + фабрику на приоритеты

--- database/seeders/PrioritySeeder.php
+++ database/seeders/PrioritySeeder.php
@@ -10,10 +10,22 @@ class PrioritySeeder extends Seeder
     public function run(): void
     {
         $rows = [
-            ['name' => 'Низкий', 'color' => '#2196F3'],
-            ['name' => 'Средний', 'color' => '#FFC107'],
-            ['name' => 'Высокий', 'color' => '#FF9800'],
-            ['name' => 'Критичный', 'color' => '#F44336'],
+            [
+                'name' => 'Низкий',
+                'color' => '#2196F3'
+            ],
+            [
+                'name' => 'Средний',
+                'color' => '#FFC107'
+            ],
+            [
+                'name' => 'Высокий',
+                'color' => '#FF9800'
+            ],
+            [
+                'name' => 'Критичный',
+                'color' => '#F44336'
+            ],
         ];
 
         foreach ($rows as $row) {
