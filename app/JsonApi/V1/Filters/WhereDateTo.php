3a9f18500216c4f3c91cfb3eb3b36af891000c77
German Boldyrev <boldyrev_german@mail.ru>
2025-10-25 14:16:09 +0300

    feat: два новых фильтра по дате

---
+++ app/JsonApi/V1/Filters/WhereDateTo.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\JsonApi\V1\Filters;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Str;
+use LaravelJsonApi\Eloquent\Contracts\Filter;
+use LaravelJsonApi\Eloquent\Filters\Concerns\DeserializesValue;
+use LaravelJsonApi\Eloquent\Filters\Concerns\IsSingular;
+
+class WhereDateTo implements Filter
+{
+    use DeserializesValue;
+    use IsSingular;
+
+    private string $name;
+    private string $column;
+
+    public static function make(string $name, string $column = null): self
+    {
+        return new static($name, $column);
+    }
+
+    public function __construct(string $name, string $column = null)
+    {
+        $this->name = $name;
+        $this->column = $column ?: Str::snake($name);
+    }
+
+    public function key(): string
+    {
+        return $this->name;
+    }
+
+    public function apply(Mixed $query, $value): Builder
+    {
+        try {
+            $date = Carbon::parse($value)->endOfDay();
+
+            return $query->where($this->column, '<=', $date);
+        } catch (\Exception $e) {
+            return $query;
+        }
+    }
+}
